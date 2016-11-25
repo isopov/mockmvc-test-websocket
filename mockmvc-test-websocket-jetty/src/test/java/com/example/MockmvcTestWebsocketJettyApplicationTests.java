d253d9356886ad0a908ad520189f222a5b7f6efd
Ivan Sopov <sopov.ivan@gmail.com>
2016-11-25 16:22:06 +0300

    first commit

---
+++ mockmvc-test-websocket-jetty/src/test/java/com/example/MockmvcTestWebsocketJettyApplicationTests.java
@@ -0,0 +1,28 @@
+package com.example;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.MockMvcPrint;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc(print = MockMvcPrint.NONE)
+public class MockmvcTestWebsocketJettyApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Test
+	public void testHello() throws Exception {
+		mockMvc.perform(get("/"))
+		.andExpect(status().isOk());
+	}
+
+}
---
+++ mockmvc-test-websocket-tomcat/src/main/java/com/example/MockmvcTestWebsocketTomcatApplication.java
@@ -0,0 +1,37 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@SpringBootApplication
+@RestController
+@EnableWebSocket
+public class MockmvcTestWebsocketTomcatApplication implements WebSocketConfigurer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockmvcTestWebsocketTomcatApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String hello() {
+		return "Hello world!";
+	}
+
+	@Override
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+		registry.addHandler(webSocketHandler(), "/ws");
+	}
+
+	@Bean
+	public WebSocketHandler webSocketHandler() {
+		return new TextWebSocketHandler();
+	}
+}
